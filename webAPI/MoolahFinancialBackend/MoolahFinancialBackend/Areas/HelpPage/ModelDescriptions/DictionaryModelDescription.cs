81226c5ac079776be194cd13bf2c320ce48aa844
Justin Hathaway <hathaway19@up.edu>
2018-10-23 02:13:56 -0700

    attempting to fix vs build issues

---
+++ webAPI/MoolahFinancialBackend/MoolahFinancialBackend/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MoolahFinancialBackend.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ webAPI/MoolahFinancialBackend/MoolahFinancialBackend/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace MoolahFinancialBackend.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
