ca5d137e264535bf4ae54de6ade4cf1ed0e41ea0
Justin Hathaway <hathaway19@up.edu>
2018-10-23 01:14:48 -0700

    created a new c# web api project to replace the old one

---
+++ webAPI/MoolahFinancialBackend/MoolahFinancialBackend/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace MoolahFinancialBackend.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
