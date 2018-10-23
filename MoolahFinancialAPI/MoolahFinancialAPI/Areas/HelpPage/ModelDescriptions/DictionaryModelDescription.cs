b084a641c150052b6186cba232ddb5d6accbd969
Justin Hathaway <hathaway19@up.edu>
2018-10-23 15:14:06 -0700

    One more attempt VS

--- MoolahFinancialAPI/MoolahFinancialAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MoolahFinancialAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- MoolahFinancialAPI/MoolahFinancialAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MoolahFinancialAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
