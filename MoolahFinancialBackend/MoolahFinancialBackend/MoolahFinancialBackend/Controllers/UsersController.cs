56f57d3e4fd07188aee6aa5424b3787196fd2136
Justin Hathaway <hathaway19@up.edu>
2018-11-06 01:18:17 -0800

    Added an Entity Controller for the User Model

---
+++ MoolahFinancialBackend/MoolahFinancialBackend/MoolahFinancialBackend/Controllers/UsersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MoolahFinancialBackend.Models;
+
+namespace MoolahFinancialBackend.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private MoolahEntities db = new MoolahEntities();
+
+        // GET: api/Users
+        public IQueryable<user> Getusers()
+        {
+            return db.users;
+        }
+
+        // GET: api/Users/5
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Getuser(int id)
+        {
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putuser(int id, user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.user_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!userExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Users
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Postuser(user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.users.Add(user);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (userExists(user.user_id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = user.user_id }, user);
+        }
+
+        // DELETE: api/Users/5
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Deleteuser(int id)
+        {
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool userExists(int id)
+        {
+            return db.users.Count(e => e.user_id == id) > 0;
+        }
+    }
+}
