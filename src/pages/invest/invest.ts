f17f907715ea6d690f6d8076aaf2502d628086b1
NavreenK <navreenkaur1@gmail.com>
2018-11-13 22:36:45 -0800

    Add a searchbar

--- src/pages/invest/invest.ts
+++ src/pages/invest/invest.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,55 @@ import { NavController } from 'ionic-angular';
 })
 export class InvestPage {
 
-  constructor(public navCtrl: NavController) {
+  public technologies : Array<any>;
+  constructor(public navCtrl: NavController) { }
+
+  ionViewDidLoad() {
+    this.declareTechnologies();
+  }
+
+  declareTechnologies() : void
+  {
+    this.technologies = [
+      {
+        name: 'Whole Foods',
+        risk: 'Moderate/Risky',
+        image: 'assets/imgs/card-amsterdam.png',
+        type: 'tech'
+      },
+      {
+        name: 'Tesla Motors',
+        risk: 'Very Risky',
+        image: 'assets/imgs/moon.jpg',
+        type: 'auto'
+      }
+    ];
+  }
+
+  filterTech(param : any) : void {
+    this.declareTechnologies();
+
+    let val : string = param;
+
+    if(val.trim() !== '')
+    {
+      this.technologies = this.technologies.filter((item) =>
+      {
+        return item.name.toLowerCase().indexOf(val.toLowerCase()) >-1 || item.risk.toLowerCase().indexOf(val.toLowerCase()) >-1;
+      })
+    }
 
   }
 
+  onFilter(category : string) : void
+  {
+    this.declareTechnologies();
+    if (category.trim() !== 'all')
+      {
+         this.technologies = this.technologies.filter((item) =>
+         {
+           return item.type.toLowerCase().indexOf(category.toLowerCase()) > -1;
+         })
+       }
+  }
 }
