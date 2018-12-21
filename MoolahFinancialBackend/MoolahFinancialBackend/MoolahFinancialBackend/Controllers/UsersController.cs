8bdfc88bbe417ef6bbf8522fced77109cf70f434
Justin Hathaway <hathaway19@up.edu>
2018-12-21 12:25:01 -1000

    Created an api to deactivate an existing user (is_deleted = true), removed the delete user api since we don't want to ever delete a user

--- MoolahFinancialBackend/MoolahFinancialBackend/MoolahFinancialBackend/Controllers/UsersController.cs
+++ MoolahFinancialBackend/MoolahFinancialBackend/MoolahFinancialBackend/Controllers/UsersController.cs
@@ -12,6 +12,8 @@ using MoolahFinancialBackend.Models;
 
 namespace MoolahFinancialBackend.Controllers
 {
+    //TODO: Create a token upon logging in. Make token required for each API
+
     /// <summary>
     /// The User Controller class.
     /// Contains all APIs for interacting with the User table.
@@ -23,6 +25,7 @@ namespace MoolahFinancialBackend.Controllers
         /// <summary>  
         /// Returns all of the users stored in the database (both deleted an non-deleted users)
         /// </summary> 
+        [HttpGet]
         [Route("api/Users")]
         public IQueryable<user> GetAllUsers()
         {
@@ -32,6 +35,7 @@ namespace MoolahFinancialBackend.Controllers
         /// <summary>  
         /// Returns all active users in the database (non-deleted users)
         /// </summary> 
+        [HttpGet]
         [Route("api/ActiveUsers")]
         public IQueryable<user> GetAllActiveUsers()
         { 
@@ -43,6 +47,7 @@ namespace MoolahFinancialBackend.Controllers
         /// </summary>  
         /// <param name="id"></param>  
         /// <returns></returns> 
+        [HttpGet]
         [ResponseType(typeof(user))]
         [Route("api/Users/{id}")]
         public IHttpActionResult GetUser(int id)
@@ -57,11 +62,38 @@ namespace MoolahFinancialBackend.Controllers
         }
 
         /// <summary>  
+        /// Sets an existing user as being deleted. This API doesn't delete a user from the table
+        /// but sets is_deleted as being true (don't want to actually delete users).
+        /// </summary>  
+        /// <param name="id"></param>  
+        /// <param name="user"></param> 
+        /// <returns></returns>
+        [HttpPut]
+        [ResponseType(typeof(void))]
+        [Route("api/Users/deactivate/{id}")]
+        public IHttpActionResult DeactivateUser(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            user user = db.users.Find(id);
+
+            user.is_deleted = true;
+
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        /// <summary>  
         /// Updates the data for an existing user.
         /// </summary>  
         /// <param name="id"></param>  
         /// <param name="user"></param> 
         /// <returns></returns> 
+        [HttpPut]
         [ResponseType(typeof(void))]
         [Route("api/Users/edit/{id}")]
         public IHttpActionResult PutUser(int id, user user)
@@ -84,7 +116,7 @@ namespace MoolahFinancialBackend.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!userExists(id))
+                if (!UserExists(id))
                 {
                     return NotFound();
                 }
@@ -98,6 +130,7 @@ namespace MoolahFinancialBackend.Controllers
         }
 
         // POST
+        [HttpPost]
         [Route("api/Users/post")]
         [ResponseType(typeof(user))]
         public IHttpActionResult PostUser(user user)
@@ -115,7 +148,7 @@ namespace MoolahFinancialBackend.Controllers
             }
             catch (DbUpdateException)
             {
-                if (userExists(user.user_id))
+                if (UserExists(user.user_id))
                 {
                     return Conflict();
                 }
@@ -128,22 +161,23 @@ namespace MoolahFinancialBackend.Controllers
             return CreatedAtRoute("DefaultApi", new { id = user.user_id }, user);
         }
 
-        // DELETE
-        [Route("api/Users/delete/{id}")]
-        [ResponseType(typeof(user))]
-        public IHttpActionResult DeleteUser(int id)
-        {
-            user user = db.users.Find(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+        //// DELETE
+        //[HttpDelete]
+        //[Route("api/Users/delete/{id}")]
+        //[ResponseType(typeof(user))]
+        //public IHttpActionResult DeleteUser(int id)
+        //{
+        //    user user = db.users.Find(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            db.users.Remove(user);
-            db.SaveChanges();
+        //    db.users.Remove(user);
+        //    db.SaveChanges();
 
-            return Ok(user);
-        }
+        //    return Ok(user);
+        //}
 
         protected override void Dispose(bool disposing)
         {
@@ -154,7 +188,7 @@ namespace MoolahFinancialBackend.Controllers
             base.Dispose(disposing);
         }
 
-        private bool userExists(int id)
+        private bool UserExists(int id)
         {
             return db.users.Count(e => e.user_id == id) > 0;
         }
