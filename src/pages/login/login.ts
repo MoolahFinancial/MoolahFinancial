f79e21a722274fc74eefc6c8c3eb7f9f9a9c1aa6
Justin Hathaway <hathaway19@up.edu>
2019-01-20 14:16:33 -0800

    Started to add validations to the login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RegisterPage } from '../register/register';
 import { TabsPage } from '../tabs/tabs';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 /**
  * Generated class for the LoginPage page.
@@ -17,10 +18,20 @@ import { TabsPage } from '../tabs/tabs';
 })
 export class LoginPage {
 
+  // Variables to hold values inside the register form
+  loginForm: FormGroup;
   username:string;
   password:string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      'email': [null, Validators.compose([Validators.required, Validators.email])],
+      'password': [null, Validators.compose([Validators.required, Validators.maxLength(255)])]
+    });
+  }
+
+  loginUser(){
+    this.navCtrl.push(TabsPage);
   }
 
   login(){
