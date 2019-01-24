c614e75e59a8706cac20a3879e7d7783ab305961
NavreenK <navreenkaur1@gmail.com>
2019-01-23 16:46:44 -0800

    Set up the investment page

--- src/pages/invest/invest.ts
+++ src/pages/invest/invest.ts
@@ -10,7 +10,7 @@ export class InvestPage {
 
   portfolio: any;
 
-  public technologies : Array<any>;
+  //public technologies : Array<any>;
   constructor(public navCtrl: NavController, public portfolioProvider: PortfolioProvider) {
     this.getPortfolios();
   }
@@ -23,64 +23,64 @@ export class InvestPage {
     });
   }
 
-  ionViewDidLoad() {
-    this.declareTechnologies();
-  }
+  //ionViewDidLoad() {
+    //this.declareTechnologies();
+  //}
 
-  declareTechnologies() : void
-  {
-    this.technologies = [
-      {
-        name: 'Whole Foods',
-        risk: 'Moderate/Risky',
-        image: 'assets/imgs/card-amsterdam.png',
-        type: 'food'
-      },
-      {
-        name: 'Tesla Motors',
-        risk: 'Very Risky',
-        image: 'assets/imgs/audi.jpg',
-        type: 'auto'
-      },
-      {
-        name: 'Facebook',
-        risk: 'Low Risk',
-        image: 'assets/imgs/work-7.jpg',
-        type: 'tech'
-      },
-      {
-        name: 'Twitter',
-        risk: 'Very Risky',
-        image: 'assets/imgs/moon.jpg',
-        type: 'tech'
-      }
-    ];
-  }
+  // declareTechnologies() : void
+  // {
+  //   this.technologies = [
+  //     {
+  //       name: 'Whole Foods',
+  //       risk: 'Moderate/Risky',
+  //       image: 'assets/imgs/card-amsterdam.png',
+  //       type: 'food'
+  //     },
+  //     {
+  //       name: 'Tesla Motors',
+  //       risk: 'Very Risky',
+  //       image: 'assets/imgs/audi.jpg',
+  //       type: 'auto'
+  //     },
+  //     {
+  //       name: 'Facebook',
+  //       risk: 'Low Risk',
+  //       image: 'assets/imgs/work-7.jpg',
+  //       type: 'tech'
+  //     },
+  //     {
+  //       name: 'Twitter',
+  //       risk: 'Very Risky',
+  //       image: 'assets/imgs/moon.jpg',
+  //       type: 'tech'
+  //     }
+  //   ];
+  // }
 
-  filterTech(param : any) : void {
-    this.declareTechnologies();
+  // filterTech(param : any) : void {
+  //   this.declareTechnologies();
+  //
+  //   let val : string = param;
+  //
+  //   if(val.trim() !== '')
+  //   {
+  //     this.technologies = this.technologies.filter((item) =>
+  //     {
+  //       return item.name.toLowerCase().indexOf(val.toLowerCase()) >-1 || item.risk.toLowerCase().indexOf(val.toLowerCase()) >-1;
+  //     })
+  //   }
+  //
+  // }
 
-    let val : string = param;
-
-    if(val.trim() !== '')
-    {
-      this.technologies = this.technologies.filter((item) =>
-      {
-        return item.name.toLowerCase().indexOf(val.toLowerCase()) >-1 || item.risk.toLowerCase().indexOf(val.toLowerCase()) >-1;
-      })
-    }
-
-  }
-
-  onFilter(category : string) : void
-  {
-    this.declareTechnologies();
-    if (category.trim() !== 'all')
-      {
-         this.technologies = this.technologies.filter((item) =>
-         {
-           return item.type.toLowerCase().indexOf(category.toLowerCase()) > -1;
-         })
-       }
-  }
-}
+//   onFilter(category : string) : void
+//   {
+//     this.declareTechnologies();
+//     if (category.trim() !== 'all')
+//       {
+//          this.technologies = this.technologies.filter((item) =>
+//          {
+//            return item.type.toLowerCase().indexOf(category.toLowerCase()) > -1;
+//          })
+//        }
+//   }
+ }
