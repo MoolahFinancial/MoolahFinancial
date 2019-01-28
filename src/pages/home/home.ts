76ae8f821c0eafd45cb7a0e1bc66ee3c40679d0b
Paige McKinney <mckinney19@up.edu>
2019-01-27 20:42:01 -0800

    Added simple doughnut chart to home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Chart } from 'chart.js';
 import { UserProvider } from '../../providers/user/user.service';
 
 @Component({
@@ -8,10 +9,39 @@ import { UserProvider } from '../../providers/user/user.service';
 })
 export class HomePage {
 
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+
   users: any;
+  doughnutChart: any;
 
   constructor(public navCtrl: NavController, public userProvider: UserProvider) {
     this.getUsers();
+  }
+
+  ionViewDidLoad() {
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+
+      type: 'doughnut',
+      data: {
+        labels: ["Snapchat", "Facebook", "Google", "Airbnb"],
+        datasets: [{
+          label: 'Portfolio Breakdown',
+          data: [5,15,17,15],
+          backgroundColor: [
+            'rgba(255,255,102,0.2)',
+            'rgba(0,102,255,0.2)',
+            'rgba(255,0,102,0.2)',
+            'rgba(255,153,102.0.2)'
+          ],
+          hoverBackgroundColor: [
+            "#FFFF66",
+            "0066FF",
+            "FF0066",
+            "FF9966"
+          ]
+        }]
+      }
+    });
   }
 
   getUsers() {
