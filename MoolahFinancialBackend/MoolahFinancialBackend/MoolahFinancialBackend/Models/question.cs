519871a288bcd4e807f9cad19d176b634166f236
Justin Hathaway <hathaway19@up.edu>
2019-02-07 15:24:34 -0800

    Added column json_possible_answers to Question table
    - string column which should store all possible answers to the question in there as a json object

--- MoolahFinancialBackend/MoolahFinancialBackend/MoolahFinancialBackend/Models/question.cs
+++ MoolahFinancialBackend/MoolahFinancialBackend/MoolahFinancialBackend/Models/question.cs
@@ -22,6 +22,7 @@ namespace MoolahFinancialBackend.Models
     
         public int question_id { get; set; }
         public string question_text { get; set; }
+        public string json_possible_answers { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<user_tag> user_tag { get; set; }
