f7dca34791c1824865aa602ef237a1882243ba00
Justin Hathaway <hathaway19@up.edu>
2019-02-21 17:02:46 -0800

    Added a method to delete a user_tag in the tag provider

--- src/providers/tag/tag.service.ts
+++ src/providers/tag/tag.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { UserTagData } from '../models';
 
 @Injectable()
@@ -21,6 +22,15 @@ export class TagProvider {
         reject(err);
       });
     });
+  }
+
+  // Calls the api to delete an existing user tag (based on the user_id & the question_text)
+  deleteUserTag(user_id: number, question_text: string): Observable<UserTagData> {
+
+    // Add the email and password params to the url for the api call
+    const params = new HttpParams().set('user_id', String(user_id)).set('question_text', question_text);
+
+    return this.http.get<UserTagData>(this.ROOT_URL + '/tags/deleteUserTag', { params } );
   }
 
 }
