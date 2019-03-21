0ffd70d598b9896513662cb4b004c141a0b3d887
Paige McKinney <mckinney19@up.edu>
2019-03-20 21:55:09 -0700

    Revert "Revert "Dynamic graphs merge""

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,8 @@ import { PortfolioProvider } from '../../providers/portfolio/portfolio.service';
 import { UserProvider } from '../../providers/user/user.service';
 import { NewsProvider } from '../../providers/news/news.service';
 import { LoginPage } from '../login/login';
+import { Portfolio, BestPortfolioInfo, Holding } from '../../providers/models';
+import { InvestPage } from '../invest/invest';
 
 @Component({
   selector: 'page-home',
@@ -20,9 +22,15 @@ export class HomePage {
   lineChart: any;
   portfolio: any;
   myPortfolio: any;
-  myTitle: any;
+  myTitle: string;
   myHoldings: any;
   news: any;
+  userPortfolio: Portfolio;
+  portfolioId: number;
+  chartLabels: [string];
+  holdingWeights: [number];
+  colors: [string];
+  hexColors: [string];
 
   constructor(public navCtrl: NavController, public app: App, public portfolioProvider: PortfolioProvider, public userProvider: UserProvider, public newsProvider: NewsProvider) {
     // If the current user is either null or undefined, return to the login screen
@@ -32,46 +40,120 @@ export class HomePage {
       nav.setRoot(LoginPage);
     } else {
       console.log(this.userProvider.currentUser, "current user on home page");
+      this.dataLoaded = false;
 
-      //this.getUsers();
-      this.getPortfolios();
+      // Initialize donut chart variables here
+      this.chartLabels = [""];
+      this.holdingWeights = [0];
+      this.colors = [""];
+      this.hexColors = [""];
+
+
       this.getNews();
 
-      //this.myPortfolio = this.portfolio[0];
-      //console.log(this.myPortfolio);
     }
   }
 
-  ionViewDidLoad() {
-    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
 
-      type: 'doughnut',
-      data: {
-        labels: ["Snapchat", "Facebook", "Google"],
-        datasets: [{
-          label: 'Portfolio Breakdown',
-          data: [5,15,10],
-          backgroundColor: [
-            'rgba(255,255,102,0.2)',
-            'rgba(0,102,255,0.2)',
-            'rgba(255,0,102,0.2)',
-          ],
-          hoverBackgroundColor: [
-            "#FFFF66",
-            "0066FF",
-            "FF0066",
-          ]
-        }]
+  ionViewDidLoad() {
+    // Get portfolio and holding information, then create doughnut chart.
+    this.getPortfolios();
+
+    // Create line chart for portfolio performance
+    this.createLineChart();
+  }
+
+  getPortfolios() {
+    this.portfolioProvider.getBestPortfolioInfo(this.userProvider.currentUser.user_id).subscribe((data: BestPortfolioInfo) => {
+
+      // Store user's portfolio ID number
+      this.portfolioId = <number>data.result[0].portfolio_id;
+
+      if (data.success && this.portfolioId != null) {
+        this.portfolioProvider.getPortfolioById(this.portfolioId).subscribe((portfolio: Portfolio) => {
+          this.userPortfolio = portfolio;
+          this.myTitle = this.userProvider.currentUser.first_name + '\'s Portfolio';
+
+          var hold: Holding;
+          var i = 0;
+
+          // Get all of the holdings in this portfolio and store them in a list of holdings
+          for (i=0; i < portfolio.holdings.length; i++) {
+            hold = <Holding>(portfolio.holdings[i]);
+
+            // Calculate an RGBA color value for each holding
+            var colorVal = 'rgba(';
+            var rgba;
+            for (var j = 0; j < 3; j++) {
+              var val = Math.floor((Math.random() *255));
+              if (j == 0) {
+                rgba = [val];
+              } else {
+                rgba.push(val);
+              }
+              colorVal += val;
+              colorVal += ',';
+            }
+            colorVal += '0.2)';
+
+            // If we're at the first element, we want to create a new list.
+            if (i==0) {
+              this.chartLabels = [hold.holding_name];
+              this.holdingWeights = [Math.round((hold.weight*100)*100)/100];
+
+              // Set the rgba color values in the chart to the random one calculated
+              this.colors = [colorVal];
+
+              // Set the hex color values in the chart to the rgba->hex color with stronger alpha value
+              this.hexColors = [this.rgbaToHexA(rgba)];
+            } else {
+              this.chartLabels.push(hold.holding_name);
+              this.holdingWeights.push(((hold.weight*100)*100)/100);
+
+              // Set the rgba color values in the chart to the random one calculated
+              this.colors.push(colorVal);
+
+              // Set the hex color values in the chart to the rgba->hex color with stronger alpha value
+              this.hexColors.push(this.rgbaToHexA(rgba));
+            }
+          }
+
+          this.createDoughnutChart();
+        });
       }
     });
+  }
 
+  createDoughnutChart() {
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: this.chartLabels,
+        datasets: [{
+          label: 'Portfolio Breakdown',
+          data: this.holdingWeights,
+          backgroundColor: this.colors,
+          hoverBackgroundColor: this.hexColors,
+        }]
+      },
+      options: {
+        title: {
+          display: true,
+          text: this.myTitle,
+          fontSize: 18,
+        },
+        maintainAspectRatio: false,
+      }
+    });
+  }
+
+  createLineChart() {
     this.lineChart = new Chart(this.lineCanvas.nativeElement, {
-
       type: 'line',
       data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        labels: ["Jan-18", "Feb-18", "Mar-18", "Apr-18", "May-18", "June-18", "July-18", "Aug-18", "Sept-18", "Oct-18", "Nov-18", "Dec-18"],
         datasets: [{
-          label: 'Portfolio Performance',
+          label: this.userProvider.currentUser.first_name + '\'s Portfolio',
           fill: false,
           lineTension: 0.1,
           backgroundColor: "rgba(75,192,192,0.4)",
@@ -89,24 +171,38 @@ export class HomePage {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [10,13,7,9,14,22,19,25,15,18,22,27],
+          data: [100,105,102,110,125,127,130,128,117,112,114,121],
           spanGaps: false,
         }]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Portfolio Performance',
+          fontSize: 18,
+        },
       }
     });
   }
 
-  getPortfolios() {
-    this.portfolioProvider.getPortfolios()
-    .then(data => {
-      this.portfolio = data;
-      console.log(this.portfolio);
-      this.myPortfolio = this.portfolio[0];
-      this.myTitle = this.myPortfolio.title;
-      this.myHoldings = this.myPortfolio.holdings;
-      console.log(this.myPortfolio);
-    });
-  }
+
+  rgbaToHexA(rgb) {
+    var r = rgb[0].toString(16);
+    var g = rgb[1].toString(16);
+    var b = rgb[2].toString(16);
+    var a = Math.round(0.5*255).toString(16);//Math.round(rgb[3] * 255).toString(16);
+
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
+    if (a.length == 1)
+      a = "0" + a;
+
+    return "#" + r + g + b + a;
+}
 
   //relevant news for portfolio
   getNews()
