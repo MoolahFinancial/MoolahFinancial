66700f9c789e5260bc48e59316c5aed2ea699b2a
Justin Hathaway <hathaway19@up.edu>
2019-03-22 10:02:21 -0700

    re-added functionality for keeping track of whether a user has filled out the Questionnaire

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -5,6 +5,8 @@ import { QuestionPage } from '../quest/quest';
 import { SECPage } from '../secforms/secforms';
 import { BankingPage } from '../banking/banking';
 import {TabsPage} from '../tabs/tabs';
+import { UserProvider } from '../../providers/user/user.service';
+import { ApiData } from '../../providers/models';
 
 @Component({
   selector: 'page-home',
@@ -12,13 +14,21 @@ import {TabsPage} from '../tabs/tabs';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController, public userProvider: UserProvider) { }
 
   itemSelected(item: string) {
     console.log("Selected Item", item);
   }
 
   continue(){
+    // Mark that the user has successfully filled out the questionnaire
+    this.userProvider.updateHasFilledOutQuestionnaire().subscribe((data: ApiData) => {
+      if(data.success) {
+        // Update the local object representing the user as well
+        this.userProvider.currentUser.has_completed_questionnaire = true;
+        console.log("User has successfully filled out the questionnaire!", this.userProvider);
+      }
+    });
     this.navCtrl.push(TabsPage);   
   }
 
